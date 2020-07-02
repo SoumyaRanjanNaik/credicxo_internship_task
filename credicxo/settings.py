ed8b4e513675d5cfb867dc1c2a0fbbc467c757c3
SoumyaRanjanNaik <srnaik2209@gmail.com>
2020-07-02 22:29:04 +0530

    final stage for production

--- credicxo/settings.py
+++ credicxo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "ka0b-tg6-*9%#j)r9%zpa-6eh(zn=b)s-^f#i3w7w0^2(!2tp0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
